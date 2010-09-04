4b7db410623c93539779a6c504160386ddc118ae
David Moore <david@davidmoore.info>
2010-09-05 11:25:45 +1200

    Added an entry point to use when starting the app; this is in anticipation of adding functionality to run the program silently from the command-line, for background scheduled tasks as an example.

---
+++ Code/IPFilter.UI/EntryPoint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace IPFilter.UI
+{
+    internal static class EntryPoint
+    {
+        [STAThread]
+        internal static void Main(string[] args)
+        {
+            var app = new App();
+
+            var window = new MainWindow();
+
+            app.Run(window);
+        }
+    }
+}
