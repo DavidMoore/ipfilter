95d146da69394026aa136debb7ddf51ea963ce6a
David Moore <david@sadrobot.co.nz>
2015-01-19 16:18:05 +1300

    Version 2.0.0.2 for issue #6

--- Code/IPFilter/Properties/AssemblyInfo.cs
+++ Code/IPFilter/Properties/AssemblyInfo.cs
@@ -18,8 +18,8 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(true)]
 
-[assembly: AssemblyInformationalVersion("2.0.0.1")]
-[assembly: AssemblyFileVersion("2.0.0.1")]
+[assembly: AssemblyInformationalVersion("2.0.0.2")]
+[assembly: AssemblyFileVersion("2.0.0.2")]
 [assembly: AssemblyVersion("2.0.0.0")]
 
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
