786ae92ab6d555ddfb96fd0322854eb29458690d
David Moore <david@davidmoore.info>
2017-05-17 18:36:06 +1200

    Start refactoring options for merging multiple lists

---
+++ Code/IPFilter/Views/OptionsWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using IPFilter.ViewModels;
+
+namespace IPFilter.Views
+{
+    /// <summary>
+    /// Interaction logic for OptionsWindow.xaml
+    /// </summary>
+    public partial class OptionsWindow
+    {
+        public OptionsWindow()
+        {
+            InitializeComponent();
+        }
+
+        public OptionsViewModel ViewModel
+        {
+            get { return DataContext as OptionsViewModel; }
+            set { DataContext = value; }
+        }
+
+        async private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
