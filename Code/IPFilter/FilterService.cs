b6b784c60dab71f9f6a3204b762e4344f4a816a2
David Moore <david@davidmoore.info>
2018-12-29 00:05:17 +1300

    Adding skeleton of feature for installing and running as a Windows service

---
+++ Code/IPFilter/FilterService.cs
@@ -0,0 +1,24 @@
+﻿using System.ServiceProcess;
+
+namespace IPFilter
+{
+    partial class FilterService : ServiceBase
+    {
+        public FilterService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            // TODO: Add code here to start your service.
+        }
+
+        protected override void OnStop()
+        {
+            // TODO: Add code here to perform any tear-down necessary to stop your service.
+        }
+
+
+    }
+}
---
+++ Code/IPFilter/FilterServiceInstaller.cs
@@ -0,0 +1,33 @@
+﻿using System.ComponentModel;
+using System.Configuration.Install;
+using System.ServiceProcess;
+
+namespace IPFilter
+{
+    [RunInstaller(true)]
+    public class FilterServiceInstaller : Installer
+    {
+        readonly ServiceProcessInstaller installer;
+        ServiceInstaller serviceInstaller;
+
+        public FilterServiceInstaller()
+        {
+            installer = new ServiceProcessInstaller
+            {
+                Account = ServiceAccount.User
+            };
+
+            serviceInstaller = new ServiceInstaller
+            {
+                DelayedAutoStart = true,
+                Description = "IPFilter Updater",
+                DisplayName = "IPFilter",
+                ServiceName = "ipfilter",
+                StartType = ServiceStartMode.Automatic
+            };
+
+            Installers.Add(serviceInstaller);
+            Installers.Add(installer);
+        }
+    }
+}
