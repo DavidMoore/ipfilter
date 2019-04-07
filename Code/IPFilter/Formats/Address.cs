b99401cb37c979750d90124b7cfcfd08e7e2cdb6
David Moore <david@davidmoore.info>
2019-04-07 17:26:21 +1200

    Adding optimized code for converting the Address structure to string, from .NET codebase

--- Code/IPFilter/Formats/Address.cs
+++ Code/IPFilter/Formats/Address.cs
@@ -24,6 +24,9 @@ namespace IPFilter.Formats
         [FieldOffset(0)]
         public readonly byte OctetD;
 
+        [FieldOffset(4)]
+        internal string StringValue;
+
         public Address(uint address) : this()
         {
             Value = address;
@@ -38,6 +41,53 @@ namespace IPFilter.Formats
         {
             return new Address(address);
         }
+
+        /// <summary>Converts an Internet address to its standard notation.</summary>
+        /// <returns>A string that contains the IP address in IPv4 dotted-quad notation.</returns>
+        public override unsafe string ToString()
+        {
+            if (StringValue != null) return StringValue;
+
+            int num1 = 15;
+            char* chPtr = stackalloc char[15];
+            int num2 = (int) (this.Value >> 24 & (long) byte.MaxValue);
+            do
+            {
+                chPtr[--num1] = (char) (48 + num2 % 10);
+                num2 /= 10;
+            } while (num2 > 0);
+
+            int num3;
+            chPtr[num3 = num1 - 1] = '.';
+            int num4 = (int) (this.Value >> 16 & (long) byte.MaxValue);
+            do
+            {
+                chPtr[--num3] = (char) (48 + num4 % 10);
+                num4 /= 10;
+            } while (num4 > 0);
+
+            int num5;
+            chPtr[num5 = num3 - 1] = '.';
+            int num6 = (int) (this.Value >> 8 & (long) byte.MaxValue);
+            do
+            {
+                chPtr[--num5] = (char) (48 + num6 % 10);
+                num6 /= 10;
+            } while (num6 > 0);
+
+            int startIndex;
+            chPtr[startIndex = num5 - 1] = '.';
+            int num7 = (int) (this.Value & (long) byte.MaxValue);
+            do
+            {
+                chPtr[--startIndex] = (char) (48 + num7 % 10);
+                num7 /= 10;
+            } while (num7 > 0);
+
+            StringValue = new string(chPtr, startIndex, 15 - startIndex);
+            return StringValue;
+        }
+
 
         /// <summary>
         /// Loopback range 127.0.0 to 127.255.255.255
