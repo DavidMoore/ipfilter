59cd63ea59b7ef92a888094d5aede3dc73d27ed1
David Moore <david@davidmoore.info>
2019-04-20 19:20:07 +1200

    More build changes

--- Code/IPFilter/Properties/AssemblyInfo.cs
+++ Code/IPFilter/Properties/AssemblyInfo.cs
@@ -6,22 +6,10 @@ using System;
 using System.Runtime.InteropServices;
 using System.Runtime.CompilerServices;
 
-[assembly: AssemblyTitle("IPFilter Updater")]
-[assembly: AssemblyDescription("IPFilter Updater")]
-[assembly: AssemblyProduct("IPFilter Updater")]
-
-[assembly: AssemblyCopyright("Copyright © David Moore 2018")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("David Moore")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en-US")]
 [assembly: ComVisible(false)]
 [assembly: CLSCompliant(false)]
-
-[assembly: AssemblyInformationalVersion("3.0.0-beta1")]
-[assembly: AssemblyFileVersion("3.0.0.0")]
-[assembly: AssemblyVersion("3.0.0.0")]
-
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: InternalsVisibleTo("IPFilter.Tests")]
