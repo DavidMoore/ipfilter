a7ec651e08f9b1d0f07aaab30b5e074502645bb6
David Moore <david@davidmoore.info>
2019-07-08 02:11:23 +1200

    A little test for the version check

---
+++ Tests/UnitTests/IPFilter.Tests/Services/SemanticVersionTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IPFilter.Services.Deployment;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace IPFilter.Tests.Services
+{
+    [TestClass]
+    public class SemanticVersionTests
+    {
+        [TestMethod]
+        public void Comparison()
+        {
+            var current = new SemanticVersion("3.0.0-beta1");
+            var newVersion = new SemanticVersion("3.0.1.4-beta");
+            Assert.IsTrue(newVersion > current);
+        }
+    }
+}
