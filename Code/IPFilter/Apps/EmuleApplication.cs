aefbe4b2e8912dc7812714d28bb05abe729b2ccf
David Moore <david@davidmoore.info>
2020-07-14 19:41:24 +1200

    Fixed #68, support eMule's different config locations

--- Code/IPFilter/Apps/EmuleApplication.cs
+++ Code/IPFilter/Apps/EmuleApplication.cs
@@ -11,45 +11,52 @@ namespace IPFilter.Apps
 {
     class EmuleApplication : IApplication
     {
+        Environment.SpecialFolder configFolder;
+
         protected virtual string RegistryKeyName => @"Software\eMule";
 
         protected virtual string DefaultDisplayName => "eMule";
 
         protected virtual string FolderName => "eMule";
-
+        
         public Task<ApplicationDetectionResult> DetectAsync()
         {
-            using (var key = Registry.CurrentUser.OpenSubKey(RegistryKeyName, false))
+            using var key = Registry.CurrentUser.OpenSubKey(RegistryKeyName, false);
+            if (key == null) return Task.FromResult(ApplicationDetectionResult.NotFound());
+
+            var installLocation = (string)key.GetValue("Install Path");
+            if (string.IsNullOrWhiteSpace(installLocation)) return Task.FromResult(ApplicationDetectionResult.NotFound());
+                
+            var result = ApplicationDetectionResult.Create(this, DefaultDisplayName, installLocation);
+                
+            var exe = new FileInfo(Path.Combine(installLocation, "emule.exe"));
+            if (!exe.Exists) result.IsPresent = false;
+
+            // eMule can be configured store config in the application folder or program data, instead of app data
+            var useSharedConfigValue = key.GetValue("UsePublicUserDirectories") ?? 0;
+            configFolder = (int) useSharedConfigValue switch
             {
-                if (key == null) return Task.FromResult(ApplicationDetectionResult.NotFound());
+                1 => Environment.SpecialFolder.CommonApplicationData,
+                2 => Environment.SpecialFolder.ProgramFilesX86,
+                _ => Environment.SpecialFolder.LocalApplicationData
+            };
 
-                var installLocation = (string)key.GetValue("Install Path");
-                if (string.IsNullOrWhiteSpace(installLocation)) return Task.FromResult(ApplicationDetectionResult.NotFound());
+            var version = FileVersionInfo.GetVersionInfo(exe.FullName);
+            result.Description = version.ProductName;
+            result.Version = version.FileVersion;
                 
-                var result = ApplicationDetectionResult.Create(this, DefaultDisplayName, installLocation);
-                
-                var exe = new FileInfo(Path.Combine(installLocation, "emule.exe"));
-                if (!exe.Exists) result.IsPresent = false;
-
-                var version = FileVersionInfo.GetVersionInfo(exe.FullName);
-                result.Description = version.ProductName;
-                result.Version = version.FileVersion;
-                
-                return Task.FromResult(result);
-            }
+            return Task.FromResult(result);
         }
 
         public async Task<FilterUpdateResult> UpdateFilterAsync(FilterDownloadResult filter, CancellationToken cancellationToken, IProgress<ProgressModel> progress)
         {
-            var roamingPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData, Environment.SpecialFolderOption.Create);
-            var destinationPath = Path.Combine(roamingPath, FolderName, "config", "ipfilter.dat");
+            var basePath = Environment.GetFolderPath( configFolder, Environment.SpecialFolderOption.Create);
+            var destinationPath = Path.Combine(basePath, FolderName, "config", "ipfilter.dat");
 
             Trace.TraceInformation("Writing filter to " + destinationPath);
-            using (var destination = File.Open(destinationPath, FileMode.Create, FileAccess.Write, FileShare.None))
-            using(var writer = new EmuleWriter(destination))
-            {
-                await writer.Write(filter.Entries, progress);
-            }
+            using var destination = File.Open(destinationPath, FileMode.Create, FileAccess.Write, FileShare.None);
+            using var writer = new EmuleWriter(destination);
+            await writer.Write(filter.Entries, progress);
 
             return new FilterUpdateResult { FilterTimestamp = filter.FilterTimestamp };
         }
